d4ef5958d30662e5a78a2fc3e0fac7594dea6b0a
Michael Akinyemi <mikedamilare@gmail.com>
2023-09-19 18:54:43 +0100

    update client_id, client_secret, and location of builders item to valid values

---
+++ lessons/click/hello.py
@@ -0,0 +1,16 @@
+import click
+
+
+@click.command(help("This tool greets you."))
+@click.option("--name", default="World", help="Who to greet.")
+@click.option("--color", default="red", help="What color to use.")
+def hello(name, color):
+    if name == "Thor":
+        click.echo(f"Hello, Thor! Your color is {color}.")
+        click.echo(click.style("Hello, {}! Your colour is {}".format(name, color), fg="red"))
+    else:
+        pass
+
+
+if __name__ == "__main__":
+    hello()
